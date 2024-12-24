6bcbb7ec3fd1273648fd95f5ec2091b92c3a7c70
Turtle1403 <giaqui140302@gmail.com>
2024-12-24 16:55:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('celebrateButton').addEventListener('click', function() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "Chúc bé có một Giáng Sinh vui vẻ và hạnh phúc!";
+    messageElement.style.opacity = 1;
+});
